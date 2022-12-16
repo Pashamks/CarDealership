b03be41ef10a8f790ffbeb65497b9118c9bdf5b8
pavlo <pavlo@DESKTOP-SM098C1>
2022-12-16 09:40:26 +0200

    Added swagger and di

---
+++ CarDealership.Repository/Repositories/CarsRepository.cs
@@ -0,0 +1,37 @@
+﻿using CarDealership.Repository.Entities;
+using CarDealership.Repository.Interfaces;
+
+namespace CarDealership.Repository.Repositories
+{
+    public class CarsRepository : ICarsRepository
+    {
+        private EfCoreDbContext GetContext()
+        {
+            return new EfCoreDbContext();
+        }
+        public Task<CarEntity> AddCar(CarEntity car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCar(CarEntity car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CarEntity> GetCarById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<CarEntity>> GetCars()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CarEntity> UpdateCar(CarEntity car)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- CarDealership/Startup.cs
+++ CarDealership/Startup.cs
@@ -1,21 +1,69 @@
+using AutoMapper;
+using CarDealership.Repository;
+using CarDealership.Repository.Interfaces;
+using CarDealership.Repository.Repositories;
+using CarDealerShip.Shared.Mapping;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
 
 namespace CarDealership
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "api/KPZ RestApi", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] { }
+                    }
+                });
+            });
+
+            services.AddMediatR(typeof(Program));
+            services.AddSingleton<ICarsRepository, CarsRepository>();
+
+            var mapConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile<MappingProfile>();
+            });
+            IMapper mapper = new Mapper(mapConfig);
+            services.AddSingleton(mapper);
+
+            var connectionStrings = Configuration.GetSection("ConnectionStrings");
+            services.AddSqlServer<EfCoreDbContext>(connectionStrings["CarsDbContext"]);
+            services.AddDbContext<EfCoreDbContext>();
+            services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,15 +73,20 @@ namespace CarDealership
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pavlo Somko RestApi v1"));
 
             app.UseRouting();
 
+            app.UseCors(x => x
+              .AllowAnyMethod()
+              .AllowAnyHeader()
+              .SetIsOriginAllowed(origin => true)
+              .AllowCredentials());
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
