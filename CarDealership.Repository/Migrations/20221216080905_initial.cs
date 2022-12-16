70fa9a6224d71284c3c0ac854047c548ba5d162b
pavlo <pavlo@DESKTOP-SM098C1>
2022-12-16 10:10:08 +0200

    added initial migration

---
+++ CarDealership.Repository/Migrations/20221216080905_initial.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarDealership.Repository.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Car",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Mark = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CreatedYear = table.Column<int>(type: "int", nullable: false),
+                    FuelTankSize = table.Column<double>(type: "float", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Car", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Purchase",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CarId = table.Column<int>(type: "int", nullable: false),
+                    BuyerName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SellerId = table.Column<int>(type: "int", nullable: false),
+                    DealPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PurchaseDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Purchase", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Seller",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SecondName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Bonuses = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Seller", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Car",
+                columns: new[] { "Id", "CreatedYear", "FuelTankSize", "Mark", "Name", "Price", "Type" },
+                values: new object[,]
+                {
+                    { 1, 2010, 2.1000000000000001, "BMV", "X3", 0m, 0 },
+                    { 2, 2012, 2.5, "BMV", "X5", 0m, 2 },
+                    { 3, 2013, 1.5, "Audi", "A3", 0m, 4 },
+                    { 4, 2014, 2.2000000000000002, "Audi", "A4", 0m, 5 },
+                    { 5, 2015, 2.6000000000000001, "Audi", "A5", 0m, 1 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Seller",
+                columns: new[] { "Id", "Bonuses", "City", "FirstName", "SecondName" },
+                values: new object[,]
+                {
+                    { 1, 0m, "Lviv", "Pavlo", "Somko" },
+                    { 2, 10m, "Lviv", "Pavlo", "Serdyuk" },
+                    { 3, 5m, "Lviv", "Ilya", "Lutsyk" },
+                    { 4, 5m, "Lviv", "Andrii", "Mykulyak" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Car");
+
+            migrationBuilder.DropTable(
+                name: "Purchase");
+
+            migrationBuilder.DropTable(
+                name: "Seller");
+        }
+    }
+}
